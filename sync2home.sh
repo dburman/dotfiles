e62d2a38ca2c0c3990ce327d6918245c040b9e6c
dburman <dburman@gmail.com>
2017-02-20 10:10:07 -0600

    fix sync2home

--- sync2home.sh
+++ sync2home.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-cp -R ~/git/dotfiles/home ~/.zsh 
-cp ~/git/dotfiles/home ~/.zshrc
-cp -R  ~/git/dotfiles/home ~/.vim
-cp ~/git/dotfiles/home ~/.vimrc
+cp -R ~/git/dotfiles/home/.zsh/* ~/.zsh/
+cp ~/git/dotfiles/home/.zshrc ~/.zshrc
+cp -R  ~/git/dotfiles/home/.vim/* ~/.vim/
+cp ~/git/dotfiles/home/.vimrc ~/.vimrc
 
