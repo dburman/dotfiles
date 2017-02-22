8ef859589869a39c2791232f0134d8105bbabac8
Douglas Burman <dburman@gmail.com>
2017-02-22 14:24:35 -0600

    update sync2home.sh

--- sync2home.sh
+++ sync2home.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-cp -R ~/git/dotfiles/home/.zsh/* ~/.zsh/
+cp -R ~/git/dotfiles/home/.zsh ~
 cp ~/git/dotfiles/home/.zshrc ~/.zshrc
-cp -R  ~/git/dotfiles/home/.vim/* ~/.vim/
+cp -R  ~/git/dotfiles/home/.vim ~
 cp ~/git/dotfiles/home/.vimrc ~/.vimrc
 
