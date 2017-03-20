114e35c2d3726efb53227dee581bcff26c4ee09a
Douglas Burman <dburman@gmail.com>
2017-03-20 10:55:19 -0500

    update path

--- home/.zsh/path.zsh
+++ home/.zsh/path.zsh
@@ -1,3 +1,3 @@
-export PATH=$PATH:$HOME/bin:/usr/local/bin:/usr/local/sbin:$HOME/.cargo/bin
+export PATH=$HOME/.cargo/bin:$HOME/bin:/usr/local/bin:/usr/local/sbin:$PATH
 #export PATH=$PATH:"/Applications/microchip/xc32/v1.31/bin"
 
