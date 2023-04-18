4c62adcc2a92362bdd2f3921a8b991f815607497
Douglas Burman <dburman@gmail.com>
2023-04-18 17:13:22 -0500

    add replace function for test find/replace in a dir

--- home/.zsh/alias.zsh
+++ home/.zsh/alias.zsh
@@ -3,7 +3,6 @@ alias ping="ping -c3"
 alias docker-cup="docker-compose pull && docker-compose up --force-recreate --build -d && docker image prune -f"
 alias lazydocker="docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock -v ~/lazydocker:/.config/jesseduffield/lazydocker lazyteam/lazydocker"
 
-#alias cgrep="grep --color=auto"
 alias :q="exit"
 alias :wq="exit"
 
@@ -16,22 +15,31 @@ if [[ $(uname) = 'Darwin' ]]; then
   alias make="make -j$((NB_CORES+1)) -l${NB_CORES}"
   if [[ -x `which brew` ]]; then alias updateos="brew update && brew upgrade && brew cleanup && mas upgrade && softwareupdate -i -a" ;fi
   alias vi="vim"
+
+  #functions....
+  replace(){ if [ $# -eq 0 ]; then echo "\nUsage:\n replace <findtext> <replacetext>\n"; return 1; else echo "\n-----------\n"; fi; rg "$1" . && echo "\n-----------\n"  && rg -l "$1" . | xargs sed -i '' "s/$1/$2/g" && rg "$2" . && echo "\n-----------\n";}
 elif [[ $(uname) = 'Linux' ]]; then
   NB_CORES=$(grep -c '^processor' /proc/cpuinfo)
   alias make="make -j$((NB_CORES+1)) -l${NB_CORES}"
-  if [[ -x `which apt` ]];
+  if [[ -x `which apt` ]]; then
     if [[ -x `which flatpak` ]]; then
-      alias updateos="sudo apt update && sudo apt upgrade && sudo apt autoremove && flatpak update";
+      alias updateos="sudo apt update && sudo apt upgrade && sudo apt autoremove && flatpak update"
     else
-      alias updateos="sudo apt update && sudo apt upgrade && sudo apt autoremove";
+      alias updateos="sudo apt update && sudo apt upgrade && sudo apt autoremove"
     fi
   fi
   if [[ -x `which vim` ]]; then alias vi="vim" ; elif [[ -x `which nvim` ]]; then alias vi="nvim" ;fi
+
+  #functions....
+  replace(){ if [ $# -eq 0 ]; then echo "\nUsage:\n replace <findtext> <replacetext>\n"; return 1; else echo "\n-----------\n"; fi; rg "$1" . && echo "\n-----------\n"  && rg -l "$1" . | xargs sed -i  "s/$1/$2/g" && rg "$2" . && echo "\n-----------\n";}
 elif [[ $(uname) = 'FreeBSD' ]]; then
   NB_CORES=$(sysctl hw.ncpu | awk '{print $2}')
   alias make="make -j$((NB_CORES+1))"
   alias updateos="sudo pkg upgrade"
   if [[ -x `which vim` ]]; then alias vi="vim" ; elif [[ -x `which nvim` ]]; then alias vi="nvim" ;fi
+
+  #functions....
+  replace(){ if [ $# -eq 0 ]; then echo "\nUsage:\n replace <findtext> <replacetext>\n"; return 1; else echo "\n-----------\n"; fi; rg "$1" . && echo "\n-----------\n"  && rg -l "$1" . | xargs sed -i '' "s/$1/$2/g" && rg "$2" . && echo "\n-----------\n";}
 fi
 
 if type -p colordiff &> /dev/null ; then
@@ -50,4 +58,8 @@ if [[ -x `which git` ]]; then
   alias gp="git pull && git submodule update --init" ;
 fi
 
+#functions....
 transfer(){ if [ $# -eq 0 ];then echo "No arguments specified.\nUsage:\n transfer <file|directory>\n ... | transfer <file_name>">&2;return 1;fi;if tty -s;then file="$1";file_name=$(basename "$file");if [ ! -e "$file" ];then echo "$file: No such file or directory">&2;return 1;fi;if [ -d "$file" ];then file_name="$file_name.zip" ,;(cd "$file"&&zip -r -q - .)|curl --progress-bar --upload-file "-" "https://xfer.muteink.com/$file_name"|tee /dev/null,;else cat "$file"|curl --progress-bar --upload-file "-" "https://xfer.muteink.com/$file_name"|tee /dev/null;fi;else file_name=$1;curl --progress-bar --upload-file "-" "https://xfer.muteink.com/$file_name"|tee /dev/null;fi;}
+
+
+
